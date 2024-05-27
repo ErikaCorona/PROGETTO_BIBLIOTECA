a3ea21a3a5f71cb1e4567c07c5dd84aa71a0a0cf
Erika Corona <ecorona1@studenti.uninsubria.it>
2024-05-27 20:00:56 +0200

    Revert "settaggi per firebase"
    This reverts commit b423f08b2c6f6dfced1141e3c18e4f13625fd725.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,4 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
-    implementation("com.google.firebase:firebase-analytics")
 }
